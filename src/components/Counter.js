abd62980425279c7afbc0b403b70e8ebf93d660c
Ziirkov <jordan.leston@epsi.fr>
2022-05-20 15:13:14 +0200

    update repo

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,7 +8,7 @@ function Counter() {
     //const [ethIn2030, setETHIn2030] = useState(0);
     //const [moneyMadeIn2030, setMoneyMadeIn2030] = useState(0);
     const [euroGainedFromLivretA, setEuroGainedFromLivretA] = useState(0);
-    const [euroGainedFromLivretAIn2030, setEuroGainedFromLivretAIn2030] = useState(0);
+    //const [euroGainedFromLivretAIn2030, setEuroGainedFromLivretAIn2030] = useState(0);
 
 
     useEffect(() => {
@@ -24,12 +24,12 @@ function Counter() {
             /**
              * Argent miné en 1 seconde * Nombre de secondes depuis le débute de l'année
              */
-            setMoneyMadeSinceStartOfTheYear(Math.round((0.00014 * startOfTheYearSeconds) * 1000) / 1000)
+            setMoneyMadeSinceStartOfTheYear((0.00014 * startOfTheYearSeconds).toFixed(4))
 
             /**
              * Nombre de secondes depuis le début de l'année * Nombre d'ETH miné en 8 seconde (arrondit à 10^-8)
              */
-            setETHSinceStartOfYear(Math.round((startOfTheYearSeconds * 0.000000069) * 100000000) / 100000000)
+            setETHSinceStartOfYear((startOfTheYearSeconds * 0.000000069).toFixed(8))
 
             /**
              * Nombre d'ETH minés en 2030 + nombre d'eth minés depuis le début de l'année 
@@ -44,12 +44,12 @@ function Counter() {
             /**
              * Taux d'intérêt du livret A depuis le début de l'année (Rendement du livret A pour 4999€ = 47.91 pour un an soit 0.0000015€ par seconde 10^-6)
              */
-            setEuroGainedFromLivretA(Math.round((startOfTheYearSeconds * 0.0000015) * 1000000) / 1000000)
+            setEuroGainedFromLivretA((startOfTheYearSeconds * 0.0000015).toFixed(6))
 
             /**
              * Taux d'intérêt du livret A en 2030 (Rendement du livret A pour 4999€ = 47.91 * 7 = 335,37 + rendement en cours pour 2030)
              */
-            setEuroGainedFromLivretAIn2030(Math.round((335.37 + euroGainedFromLivretA + 0.0000015) * 1000000) / 1000000)
+            //setEuroGainedFromLivretAIn2030(Math.round((335.37 + euroGainedFromLivretA + 0.0000015) * 1000000) / 1000000)
         }, 1000);
 
         return () => {
@@ -60,11 +60,14 @@ function Counter() {
     return (
         <div className='bg-image'>
             <div className='counter'>
-                <h1 className='title'>AVEC UN RIG A 4999€ :</h1>
-                <h2 className='paragraph'>Vous auriez miné l'équivalent de {ethSinceStartOfTheYear} ETH soit {moneyMadeSinceStartOfTheYear} € depuis le début de l'année</h2>
-                <h2 className='paragraph'>Si vous conservez vos ETH et que ETH continue la même croissance en 2030 vous aurez miné 16.15266644 ETH soit 403816.66€</h2>
-                <h2 className='paragraph'>En ouvrant un livret A avec 4999€ vous auriez généré l'équivalent de {euroGainedFromLivretA}€ depuis le début de l'année</h2>
-                <h2 className='paragraph'>En ouvrant un livret A avec 4999€ vous auriez généré l'équivalent de {euroGainedFromLivretAIn2030}€ EN 2030</h2>
+                <div className='rig'>
+                    <h2 className='paragraph' id='paragraph1'><span className='important'>{ethSinceStartOfTheYear}</span> ETH <br></br> SOIT {moneyMadeSinceStartOfTheYear}€ </h2>
+                    <h2 className='paragraph' id='paragraph2'> <span className='important'>16.15266</span> ETH <br></br> SOIT 403816.66€</h2>
+                </div>
+                <div className='livret'>                
+                    <h2 className='paragraph' id='paragraph3'><span className='important'>{euroGainedFromLivretA}€</span> D'INTÉRÊTS</h2>
+                    <h2 className='paragraph' id='paragraph4'><span className='important'>383.28€</span>€ D'INTÉRÊTS</h2>
+                </div>
             </div>
         </div>
     );
